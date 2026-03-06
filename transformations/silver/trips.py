690a01ce2eef2577c5ee495da19c344d05c0f830
Ousseynou Dione <dioneousseynou03@gmail.com>
2026-03-06 23:56:10 +0000

    add transformed trips

--- transformations/silver/trips.py
+++ transformations/silver/trips.py
@@ -1,13 +1,9 @@
 from pyspark import pipelines as dp
-from pyspark import functions as F
-
+from pyspark.sql import functions as F
 
 @dp.view(
     name = "Trips_silver_staging",
     comment = "Transformed trips data ready for CDC upsert"
-    table_properties = {
-        
-    }
 )
 @dp.expect("valid_data", "year(business_date) >= 2020")
 @dp.expect("valid_driver_rating", "driver_rating BETWEEN 1 AND 10")
@@ -16,17 +12,16 @@ def trips_silver():
     df_bronze = spark.readStream.table("transportation.bronze.trips")
 
     df_silver = df_bronze.select(
-        F.col("trip_id").alias("id")
-        F.col("date").cast("date").alias("date")
-        F.col("city_id").alias("city_id")
-        F.col("passenger_type").alias("passenger_category")
-        F.col("distance_travelled_km").alias("distance_kms")
-        F.col("fare_amount").alias("sales_amt")
-        F.col("passenger_rating").alias("passenger_rating")
-        F.col("driver_rating").alias("driver_rating")
-        F.col("ingest_date").alias("bronze_ingest_timestamp")
+        F.col("trip_id").alias("id"),
+        F.col("date").cast("date").alias("business_date"),
+        F.col("city_id").alias("city_id"),
+        F.col("passenger_type").alias("passenger_category"),
+        F.col("distance_travelled_km").alias("distance_kms"),
+        F.col("fare_amount").alias("sales_amt"),
+        F.col("passenger_rating").alias("passenger_rating"),
+        F.col("driver_rating").alias("driver_rating"),
+        F.col("ingest_date").alias("bronze_ingest_timestamp"),
     )
-
 
     df_silver = df_silver.withColumn("silver_processed_timestamp", F.current_timestamp())
 
@@ -50,5 +45,5 @@ dp.create_auto_cdc_flow(
     keys = ["id"],
     sequence_by = F.col("silver_processed_timestamp"),
     stored_as_scd_type = 1,
-    excep_column_list = []
+    except_column_list = []
 )
